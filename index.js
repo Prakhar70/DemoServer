a76d5be498c718650271bdcf23dd8b42d1fd03ff
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-12-19 22:08:39 +0530

    simple server

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 const ip = require('ip');
-const os = require('os');
 
 
 app.get('/ping',(req, res)=>{
     return res.json({
         message: 'pong',
         serverAddress: ip.address(),
-        //subnet: ip.subnet(ip.address(), ip.toLong(ip.address())),
-        interfaces:os.networkInterfaces()
     })
 })
 
